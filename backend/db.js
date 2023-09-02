98a266580f4a8e624212c7d20411441f8f86ec9b
NIKHIL GUPTA <nikhil20530@iiitd.ac.in>
2023-09-02 12:09:18 +0530

    commit

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/"
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+};
+
+module.exports = connectToMongo;
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import Main from './Pages/AdminStudentList';
+function App() {
+  return (
+    <>
+   <Main/>
+    </>
+  );
+}
+
+export default App
