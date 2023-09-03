5a2a6d64cc1125c115ade84592421e61b76dbe4b
NIKHIL GUPTA <nikhil20530@iiitd.ac.in>
2023-09-03 23:46:46 +0530

    Round1 - Frontend Done

---
+++ src/Components/AdminStudent.jsx
@@ -0,0 +1,54 @@
+import React, { useContext, useEffect } from 'react';
+import StudentContext from '../context/StudentContext'; // Import the StudentContext
+
+const Tablestudents = () => {
+  // Access the student data from the context
+  const { students } = useContext(StudentContext);
+
+  useEffect(() => {
+    console.log(students[0]);
+  }, [students]);
+
+  const renderHeaderRow = () => {
+    if (students.length === 0) {
+      // Render the header row with "XLSX Data" when there are no students
+      return (
+        <tr>
+          <th className="bg-[#3dafaa] text-center font-bold p-2 text-white">
+            XLSX Data
+          </th>
+        </tr>
+      );
+    } else {
+      // Render the header row using students[0] when students are available
+      return (
+        <tr className="bg-[#3dafaa] text-white">
+          {Object.values(students[0]).map((data, index) => (
+            <th className='border p-2 text-center' key={index}>{data}</th>
+          ))}
+        </tr>
+      );
+    }
+  };
+
+  return (
+    <div>
+      <table className="w-full border-collapse border mt-4">
+        <thead>
+          {renderHeaderRow()}
+        </thead>
+        <tbody>
+          {students.slice(1).map((student, index) => (
+            <tr className='text-center' key={index}>
+              {Object.values(student).map((data, ind) => (
+                <td className='border p-2' key={ind}>{data}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Tablestudents;
---
+++ src/Components/CoursePage.jsx
@@ -0,0 +1,104 @@
+import React, { useContext, useState, useEffect } from "react";
+import CourseContext from "../context/CourseContext";
+import StudentContext from "../context/StudentContext";
+import DepartmentContext from "../context/DepartmentContext";
+
+const CoursePage = () => {
+  const { selectedCourse } = useContext(CourseContext);
+  const { students, allocateStudent } = useContext(StudentContext);
+  const { selectedDepartment } = useContext(DepartmentContext);
+  const [clickedStudentName, setClickedStudentName] = useState(null);
+
+  const allocatedStudents = students.filter(
+    (student) =>
+    student[2] === selectedDepartment && 
+    student[3] === "Yes" && 
+    student[4] === selectedCourse
+  );
+
+  const availableStudents = students.filter(
+    (student) =>
+      student[2] === selectedDepartment && 
+      student[3] === "No" 
+  );
+
+  const handleStudentIdClick = (id) => {
+
+    console.log(id)
+    allocateStudent(id,selectedCourse);
+    setClickedStudentName(id);
+  };
+
+
+  useEffect(() => {
+    console.log(availableStudents)
+  }, [students]);
+
+  return (
+    <div>
+      <h1 className="text-[#3dafaa] text-3xl font-bold m-5">
+        {selectedCourse}
+      </h1>
+
+      <div className="m-5">
+        <h2 className="text-[#3dafaa] text-2xl font-bold mb-2">
+          Allocated Students
+        </h2>
+        <table className="w-full border-collapse border">
+          <thead>
+            <tr className="bg-[#3dafaa] text-white">
+              <th className="border p-2 text-center">ID</th>
+              <th className="border p-2 text-center">Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allocatedStudents.map((student, index) => (
+              <tr className="text-center" key={index}>
+                <td className="border p-2">{student[0]}</td>
+                <td className="border p-2">
+                  <button
+                    onClick={() => handleStudentIdClick(student[0])}
+                    className="bg-[#3dafaa] text-white px-4 py-2 rounded cursor-pointer font-bold"
+                  >
+                    {student[1]}
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="m-5">
+        <h2 className="text-[#3dafaa] text-2xl font-bold mb-2">
+          Available Students
+        </h2>
+        <table className="w-full border-collapse border">
+          <thead>
+            <tr className="bg-[#3dafaa] text-white">
+              <th className="border p-2 text-center">ID</th>
+              <th className="border p-2 text-center">Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {availableStudents.map((student, index) => (
+              <tr className="text-center" key={index}>
+                <td className="border p-2">{student[0]}</td>
+                <td className="border p-2">
+                  <button
+                    onClick={() => handleStudentIdClick(student[0])}
+                    className="bg-[#3dafaa] text-white px-4 py-2 rounded cursor-pointer font-bold"
+                  >
+                    {student[1]}
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default CoursePage;
