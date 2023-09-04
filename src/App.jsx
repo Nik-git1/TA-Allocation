22efcf8cdc3384d90d92e120adb582200c749a7a
NIKHIL GUPTA <nikhil20530@iiitd.ac.in>
2023-09-04 14:24:36 +0530

    clean app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 
-import Course from './Pages/CourseView';
-import Department from './Components/DeptView';
 import Admin from './Pages/AdminPage';
 
 import StudentState from './context/StudentState';
@@ -17,8 +12,7 @@ function App() {
     <DepartmentState>
       <CourseState>
         <StudentState>
-          {/* <Department header={Header} row={Row} /> */}
-          {/* <Course header={Header} row={Row} /> */}
+
           <Admin />
         </StudentState>
       </CourseState>
