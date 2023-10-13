847f22d759c720061e69ea4f2c9d54c7bfe2fd20
Arnav-KUM <arnav20363@iiitd.ac.in>
2023-10-13 18:06:23 +0530

    Removing Professor validation

--- backend/models/Professor.js
+++ backend/models/Professor.js
@@ -8,29 +8,29 @@ const professorSchema = new mongoose.Schema(
             required: true,
             unique: true,
             match: [ /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, `Please fill valid email address` ],
-            validate: {
-                validator: function ()
-                {
-                    return new Promise( ( res, rej ) =>
-                    {
-                        User.findOne( { email: this.email, _id: { $ne: this._id } } )
-                            .then( data =>
-                            {
-                                if ( data )
-                                {
-                                    res( false )
-                                } else
-                                {
-                                    res( true )
-                                }
-                            } )
-                            .catch( err =>
-                            {
-                                res( false )
-                            } )
-                    } )
-                }, message: 'Email Already Taken'
-            },
+            // validate: {
+            //     validator: function ()
+            //     {
+            //         return new Promise( ( res, rej ) =>
+            //         {
+            //             User.findOne( { email: this.email, _id: { $ne: this._id } } )
+            //                 .then( data =>
+            //                 {
+            //                     if ( data )
+            //                     {
+            //                         res( false )
+            //                     } else
+            //                     {
+            //                         res( true )
+            //                     }
+            //                 } )
+            //                 .catch( err =>
+            //                 {
+            //                     res( false )
+            //                 } )
+            //         } )
+            //     }, message: 'Email Already Taken'
+            // },
         },
         password: {
             type: String,
