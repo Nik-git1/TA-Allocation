2e6449b52e6eacae907dcfd44b7c632191c88efe
Atyant Sony <atyant20039@iiitd.ac.in>
2024-04-23 01:10:44 +0530

    removed department tab

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const SideBar = () => {
   const handleLogout = () => {
@@ -7,47 +7,68 @@ const SideBar = () => {
     window.location.replace("http://localhost:5173");
   };
   return (
-    <div className='bg-[#3dafaa] h-screen text-center max-w-[95%] mt-4'>
-      <div className='flex flex-col'>
-        <Link to="/admin/" className='bg-[#3dafaa] p-2 h-16 hover:bg-[rgb(50,140,135)] focus:bg-[rgb(50,140,135)] text-white font-bold'>
+    <div className="bg-[#3dafaa] h-screen text-center max-w-[95%] mt-4">
+      <div className="flex flex-col">
+        <Link
+          to="/admin/"
+          className="bg-[#3dafaa] p-2 h-16 hover:bg-[rgb(50,140,135)] focus:bg-[rgb(50,140,135)] text-white font-bold"
+        >
           Dashboard
         </Link>
-        <hr className='border-t-2' />
-        <Link to="/admin/student" className='bg-[#3dafaa] h-16 p-2 hover:bg-[rgb(50,140,135)] focus:bg-[rgb(50,140,135)] text-white font-bold'>
+        <hr className="border-t-2" />
+        <Link
+          to="/admin/student"
+          className="bg-[#3dafaa] h-16 p-2 hover:bg-[rgb(50,140,135)] focus:bg-[rgb(50,140,135)] text-white font-bold"
+        >
           Students
         </Link>
-        <hr className='border-t-2' />
-        <Link to="/admin/course" className='bg-[#3dafaa] p-2 h-16 hover:bg-[rgb(50,140,135)] focus:bg-[rgb(50,140,135)] text-white font-bold'>
+        <hr className="border-t-2" />
+        <Link
+          to="/admin/course"
+          className="bg-[#3dafaa] p-2 h-16 hover:bg-[rgb(50,140,135)] focus:bg-[rgb(50,140,135)] text-white font-bold"
+        >
           Courses
         </Link>
-        <hr className='border-t-2' />
-        <Link to="/admin/department" className='bg-[#3dafaa] p-2 h-16 hover:bg-[rgb(50,140,135)] focus:bg-[rgb(50,140,135)] text-white font-bold'>
+        <hr className="border-t-2" />
+        <Link
+          to="/admin/department"
+          className="bg-[#3dafaa] p-2 h-16 hover:bg-[rgb(50,140,135)] focus:bg-[rgb(50,140,135)] text-white font-bold"
+        >
           Allocate
-        </Link> 
-        <hr className='border-t-2' />
-        <Link to="/admin/professors" className='bg-[#3dafaa] p-2 h-16 hover:bg-[rgb(50,140,135)] focus:bg-[rgb(50,140,135)] text-white font-bold'>
+        </Link>
+        <hr className="border-t-2" />
+        <Link
+          to="/admin/professors"
+          className="bg-[#3dafaa] p-2 h-16 hover:bg-[rgb(50,140,135)] focus:bg-[rgb(50,140,135)] text-white font-bold"
+        >
           Faculty
         </Link>
-        <hr className='border-t-2' />
+        {/* <hr className='border-t-2' />
         <Link to="/admin/jms" className='bg-[#3dafaa] p-2 h-16 hover:bg-[rgb(50,140,135)] focus:bg-[rgb(50,140,135)] text-white font-bold'>
           Departments
-        </Link>
-        <hr className='border-t-2' />
-        <Link to="/admin/logs" className='bg-[#3dafaa] p-2 h-16 hover:bg-[rgb(50,140,135)] focus:bg-[rgb(50,140,135)] text-white font-bold'>
+        </Link> */}
+        <hr className="border-t-2" />
+        <Link
+          to="/admin/logs"
+          className="bg-[#3dafaa] p-2 h-16 hover:bg-[rgb(50,140,135)] focus:bg-[rgb(50,140,135)] text-white font-bold"
+        >
           Logs
         </Link>
-        <hr className='border-t-2' />
-        <Link to="/admin/feedback" className='bg-[#3dafaa] p-2 h-16 hover:bg-[rgb(50,140,135)] focus:bg-[rgb(50,140,135)] text-white font-bold'>
+        <hr className="border-t-2" />
+        <Link
+          to="/admin/feedback"
+          className="bg-[#3dafaa] p-2 h-16 hover:bg-[rgb(50,140,135)] focus:bg-[rgb(50,140,135)] text-white font-bold"
+        >
           Feedback
         </Link>
-        <hr className='border-t-2' />
-        <button className='bg-[#3dafaa] p-2 h-16 hover:bg-[rgb(50,140,135)] focus:bg-[rgb(50,140,135)] text-white font-bold'
-        onClick = {handleLogout}
+        <hr className="border-t-2" />
+        <button
+          className="bg-[#3dafaa] p-2 h-16 hover:bg-[rgb(50,140,135)] focus:bg-[rgb(50,140,135)] text-white font-bold"
+          onClick={handleLogout}
         >
           Logout
         </button>
-        <hr className='border-t-2' />
-
+        <hr className="border-t-2" />
       </div>
     </div>
   );
